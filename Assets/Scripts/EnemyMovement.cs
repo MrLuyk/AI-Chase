ab1b992c03b8c03f86dd6d30ab4ff372776ef676
Mr. Luyk <133881094+MrLuyk@users.noreply.github.com>
2026-03-13 14:48:54 -0400

    Restarted enemy scripts. Enemy now moves forward and turns towards first pip the raycast hits. began second raycast hit.

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class RaycastExample : MonoBehaviour
 {
-    public float rayDistance = 10f;
+    public float rayDistance = 100f;
+    private string lastWall;
+    public float movementSpeed = 5f;
+    public float rotationSpeed = 90f;
 
     void Update()
     {
-        Vector2 origin = transform.position + transform.right * .75f;
-        Vector2 direction = transform.right;
+        Vector2 origin = transform.position + transform.up * -2.25f;
+        Vector2 direction = -transform.up;
 
         RaycastHit2D hit = Physics2D.Raycast(origin, direction, rayDistance);
+        RaycastHit2D hit2 = Physics2D.Raycast(origin, direction, rayDistance);
 
         Debug.DrawRay(origin, direction * rayDistance, Color.green);
 
@@ -19,6 +23,19 @@ public class RaycastExample : MonoBehaviour
         {
             Debug.Log("Raycast hit: " + hit.collider.name + " at point: " + hit.point);
             Debug.DrawRay(origin, direction * hit.distance, Color.red);
+
+            if (hit.collider.gameObject.CompareTag("Wall"))
+            {
+                Debug.Log(hit.collider.name);
+                transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+                transform.position += (Vector3)direction * movementSpeed * Time.deltaTime;
+            }
+
+            if (hit.collider.gameObject.CompareTag("PatrolPoint"))
+            {
+                Debug.Log(hit.collider.name);
+                transform.position += (Vector3)direction * movementSpeed * Time.deltaTime;
+            }
         }
         else
         {
